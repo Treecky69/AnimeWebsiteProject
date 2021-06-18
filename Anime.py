f4bce41b1ba6b9d940632abd54785741559c8735
Treecky69 <filipthedank2407@gmail.com>
2021-06-18 18:17:47 +0200

    first commit lol

---
+++ Anime.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, blueprints
+from AnimeWebsiteProject import app, home_view
+import sqlite3, json, pprint
+
+#home directory, main page where random anime genres will be displayed
+
+@home_view.route("/") 
+@home_view.route("/home")
+def index():
+    return render_template("main_page.html")
+
+#Anime Table
+
+@home_view.route("/anime_list")
+def anime_list():
+    con = sqlite3.connect("anime_mock.db")  
+    con.row_factory = sqlite3.Row  
+    cur = con.cursor()
+    cur.execute("""select anime.name as anime_name, anime.numepisodes as numepisodes, anime.rating as rating, genres.genre as anime_genre from anime_genre 
+    inner join anime on anime.ID = anime_genre.animeID inner join genres on genres.ID = anime_genre.genreID""")
+    rows = cur.fetchall()
+    return render_template("anime_table.html", rows = rows)
+
+#Runnning the server
+
+if __name__ == "__main__":
+    app.register_blueprint(home_view) # register blueprint
+    app.config.from_pyfile("settingslocal.py") #config file
+    #app.config["EXPLAIN_TEMPLATE_LOADING"] = True #debug in the command line, tells you where it searches for the templates and static files
+    app.run(debug = True, port = 6969)
---
+++ AnimeWebsiteProject/__init__.py
@@ -0,0 +1,9 @@
+"""
+The flask application package.
+"""
+
+from flask import Flask
+from flask import *
+#import AnimeWebsiteProject.views
+app = Flask(__name__, static_folder=".\\looks\\static")
+home_view = Blueprint("home_view", __name__, template_folder=".\\looks\\templates")
