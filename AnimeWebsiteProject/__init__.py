9164cc6de764acaaf562186b7efec8c1bbb17a76
Treecky69 <filipthedank2407@gmail.com>
2021-08-01 21:01:49 +0200

    linux compatible

--- AnimeWebsiteProject/__init__.py
+++ AnimeWebsiteProject/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import *
+from sys import platform
 #import AnimeWebsiteProject.views
 app = Flask(__name__)
 user = Blueprint("user", __name__)
-admin = Blueprint("admin", __name__, template_folder=".\\templates\\admin")
+if platform == "linux":
+    admin = Blueprint("admin", __name__, template_folder="./templates/admin")
+if platform == "win32":
+    admin = Blueprint("admin", __name__, template_folder=".\\templates\\admin")
